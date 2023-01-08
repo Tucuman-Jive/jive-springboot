6efed0e09a666a75bdfc937d9c9b88ca21c1b08b
Carolina Diazgranados <linacello@gmail.com>
2023-01-08 10:35:23 -0500

    First commit

---
+++ src/main/java/rocks/zipcode/Jive/JiveApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.Jive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/zipcode/Jive/JiveApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.Jive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
