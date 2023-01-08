d7d915a7750d14d985dbb6d01b09906dc6e5db50
Carolina Diazgranados <linacello@gmail.com>
2023-01-08 11:43:40 -0500

    added annotations to user entity

--- src/main/java/rocks/zipcode/Jive/repositories/UserRepository.java
+++ src/main/java/rocks/zipcode/Jive/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package rocks.zipcode.Jive.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rocks.zipcode.Jive.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
