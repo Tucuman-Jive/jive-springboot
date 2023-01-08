a8fc3e4a8de4f5e83eb7a1ee1da4615c02c98274
Carolina Diazgranados <linacello@gmail.com>
2023-01-08 12:59:25 -0500

    added user crud

--- src/main/java/rocks/zipcode/Jive/controllers/UserController.java
+++ src/main/java/rocks/zipcode/Jive/controllers/UserController.java
@@ -1,4 +1,59 @@
 package rocks.zipcode.Jive.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rocks.zipcode.Jive.entities.User;
+import rocks.zipcode.Jive.services.UserService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/add")
+    public String addUser(@RequestBody User user) {
+       userService.saveUser(user);
+       return "User has been saved";
+    }
+
+
+    @GetMapping("/all")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{idUser}")
+    public ResponseEntity<User> getUserById(@PathVariable Long idUser) {
+        try {
+            User user = userService.getUserById(idUser);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{idUser}")
+    public ResponseEntity<User> update(@RequestBody User user, @PathVariable Long idUser) {
+        try {
+            userService.saveUser(user); //why no id?
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{idUser}")
+    public String delete(@PathVariable Long idUser) {
+        userService.deleteUserById(idUser);
+        return "User " + idUser + " has been deleted.";
+    }
+
 }
