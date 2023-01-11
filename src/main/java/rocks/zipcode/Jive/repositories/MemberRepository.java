cd62ffe4f10b1e0aa966ad991831fd6e63940006
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2023-01-11 10:28:19 -0500

    Added an endpoint for getting memberships by User Id

--- src/main/java/rocks/zipcode/Jive/repositories/MemberRepository.java
+++ src/main/java/rocks/zipcode/Jive/repositories/MemberRepository.java
@@ -1,6 +1,5 @@
 package rocks.zipcode.Jive.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import rocks.zipcode.Jive.entities.Membership;
@@ -10,6 +9,8 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends CrudRepository<Membership, Long> {
     List<Membership> findByChannelId(Long id); // do this for messages?
+
+    List<Membership> getByUserEntityId(Long id);
 
     List<Membership> findAll();
 }
