fbaff5b9e63bb2c3ba124879cf6a511d56b9c259
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2023-01-11 23:39:05 -0500

    Added message and messagebox components to the front-end

---
+++ webapp/src/components/messageBox/messagebox.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function MessageBox() {
+  const [user, setUser] = useState([]);
+  const [message, setMessage] = useState("");
+  const [channel, setChannel] = useState([]);
+  const [payload, setPayload] = useState("");
+
+  const userUrl = "http://localhost:8080/users/1";
+  const postMessageUrl = "http://localhost:8080/messages/add/user/1/channel/1";
+  const channelUrl = "http://localhost:8080/channels/1";
+
+  const loadUser = async () => {
+    const result = await axios.get(userUrl);
+    setUser(result.data);
+  };
+
+  const loadChannel = async () => {
+    const result = await axios.get(channelUrl);
+    setChannel(result.data);
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  useEffect(() => {
+    loadChannel();
+  }, []);
+
+  const options = {
+    url: postMessageUrl,
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    data: {
+      message: message,
+      channel: {
+        id: channel.id,
+        name: channel.name,
+      },
+      userEntity: {
+        id: user.id,
+        userName: user.userName,
+      },
+    },
+  };
+
+  const postMessage = () =>
+    axios(options).then((response) => {
+      console.log(response.status);
+    });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setPayload(message.toString);
+    postMessage();
+  };
+
+  const renderBox = () => {
+    return (
+      <form>
+        <textarea
+          value={message}
+          required
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button onClick={handleSubmit}>Post</button>
+      </form>
+    );
+  };
+
+  return <div>{renderBox()}</div>;
+}
---
+++ webapp/src/components/messages/messages.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./messages.css";
+import "../messageBox/messagebox";
+import MessageBox from "../messageBox/messagebox";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Messages() {
+  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState([]);
+  const [message, setMessage] = useState("");
+  const [channel, setChannel] = useState([]);
+
+  const messagesUrl = "http://localhost:8080/messages/all/channel/1";
+  const userUrl = "http://localhost:8080/users/1";
+  const postMessageUrl = "http://localhost:8080/messages/add/user/1/channel/1";
+  const channelUrl = "http://localhost:8080/channels/1";
+
+  const loadMessages = async () => {
+    const result = await axios.get(messagesUrl);
+    setMessages(result.data);
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(userUrl);
+    setUser(result.data);
+  };
+
+  const loadChannel = async () => {
+    const result = await axios.get(channelUrl);
+    setChannel(result.data);
+  };
+
+  useEffect(() => {
+    loadMessages();
+  }, []);
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  useEffect(() => {
+    loadChannel();
+  }, []);
+
+  const renderMessages = messages.map((message) => {
+    if (message.userEntity.id === user.id)
+      return (
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col>
+              <div className="message_right">
+                <p>
+                  <strong>{message.message}</strong>
+                </p>
+                <p>me!</p>
+                <br />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      );
+    return (
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col>
+            <div className="message_left">
+              <strong>{message.message}</strong>
+              <p>{message.userEntity.userName}</p>
+              <br />
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    );
+  });
+
+  return (
+    <div>
+      {renderMessages}
+      <MessageBox />
+    </div>
+  );
+}
